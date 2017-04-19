f4e0f363c38b6c34700a61c310e9428d4417091f
Chandnika <chandnika55@gmail.com>
2017-04-19 19:24:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,44 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+//obj for a1-- has the values
+var a1={
+    title:'a1',
+    heading:'a11',
+    date:'April19,2017|||CS Lab',
+    content:`<p> Okay</p>
+    <p> Okay</p>
+    <p> Okay</p>
+    <p> Okay</p> `
+    
+    
+  
+};
+function f(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+var ht=
+`<html>
+<head>
+
+<title>
+${title}
+</title>
+</head>
+<body>
+<h1> ${heading} </h1>
+<div> <p>  ${date} </p></div>
+
+<p> {content} </p>
+</body>
+</html>`
+;
+return ht;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -11,7 +49,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res)
 {
-res.sendFile(path.join(__dirname,'ui','article-one.html'));
+res.send(f(a1));
 });
 app.get('/article-two',function(req,res)
 {
