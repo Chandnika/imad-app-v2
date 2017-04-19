773f8fc555896d6d9573d5f4503ae5dbc7ed0489
Chandnika <chandnika55@gmail.com>
2017-04-19 17:17:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res)
+{
+res.send('Okay, So what? ');
+});
+app.get('/article-one',function(req,res)
+{
+res.send('Apdiyaaaaaaaaaaaaaaaa ');
+});
+app.get('/article-one',function(req,res)
+{
+res.send('Ahaan ');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
